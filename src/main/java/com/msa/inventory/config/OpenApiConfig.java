c7c8b9ab762ec9e4c315c10ab0628c4b8e20d0b1
Nilesh <nilsstudy29@gmail.com>
2026-01-04 11:48:46 +0530

    Updated README, Added APIs, Swagger Configs, Placeholder for Liquibase

---
+++ src/main/java/com/msa/inventory/config/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package com.msa.inventory.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Inventory Service API",
+                version = "v1",
+                description = "APIs for inventory-service"
+        )
+)
+public class OpenApiConfig {
+}
---
+++ src/main/java/com/msa/inventory/controller/InventoryController.java
@@ -0,0 +1,22 @@
+package com.msa.inventory.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+    @Operation(
+            description = "Health Check",
+            summary = "Return 200"
+    )
+    @GetMapping(value = "/health", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> health(){
+        return ResponseEntity.ok("Working fine in Inventory");
+    }
+}
